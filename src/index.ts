b5e05c0fb26810c15a1b45fecf06b2ef6da9babd
Riza Fahmi <rizafahmi@gmail.com>
2024-08-12 18:17:19 +0700

    Basic layout and css

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
+import { Hono } from "hono";
+import fs from "node:fs";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use("/static/*", serveStatic({ root: "/" }));
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
+app.get("/", (c) => {
+  return c.html(fs.readFileSync("src/index.html", "utf8"));
+});
+
+const port = 3000;
+console.log(`Server is running on port ${port}`);
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
