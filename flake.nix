b763222b64fe4deb9688358bdaea18411ec6ebf4
Riza Fahmi <rizafahmi@gmail.com>
2024-08-15 09:35:57 +0700

    Add turso via nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A Nix-flake-based development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self , nixpkgs ,... }: let
+    # system should match the system you are running on
+    # system = "x86_64-linux";
+    system = "aarch64-darwin";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      # create an environment with nodejs_18
+      packages = with pkgs; [
+        turso-cli
+      ];
+
+    };
+  };
+}
